e6afa0bdf8239aa2169f78f3e7ef037acce2fdd4
Alexandru Moise <alexandru.t.moise@gmail.com>
2023-01-18 20:18:46 +0100

    try to add glfw to cmake

---
+++ src/include/application.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <GLFW/glfw3.h>
+
+using namespace std;
+
+class Application
+{
+public:
+    unsigned int var1;
+    Glfw glfw;
+    Window window;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,4 @@
-#include <iostream>
-
-using namespace std;
+#include "application.hpp"
 
 int main() {
 	cout<<"Hello world"<<endl;
